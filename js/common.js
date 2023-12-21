9b692633099740cda63eb5b1f393e1e6b88e8f6a
symoon96 <symoon96@gmail.com>
2023-12-21 09:12:52 +0900

    반응형 작업진행

--- js/common.js
+++ js/common.js
@@ -4,14 +4,51 @@ $(document).ready(function(){
     gnb();
 })
 
+$(window).resize(function(){
+    gnb();
+})
+
 function gnb(){
     windowWidth = $(window).innerWidth();
 
-    if(windowWidth > 1440) {
+    if(windowWidth > 1480) {
         $('.gnb .depth01 > ul > li').hover(function(){
-            $(this).addClass('hover')
+            if(!$('.gnb').hasClass('active')){
+                $(this).addClass('hover')
+            }
         }, function(){
             $(this).removeClass('hover')
+        })
+        
+
+        $('.btn-menu').click(function(){
+            if(!$(this).hasClass('active')){
+                $(this).addClass('active');
+    
+                $('.gnb').addClass('active');
+
+                setTimeout(() => {
+                    $('.gnb.active .depth01 > ul > li > .depth02').css('opacity', '1')
+                    $('.gnb .bg-area').css('opacity', '1')
+                }, 100);
+            } else {
+                $('.gnb.active .depth01 > ul > li > .depth02').css('opacity', '0');
+                $('.gnb .bg-area').css('opacity', '0');
+
+                $(this).removeClass('active');
+                $('.gnb').removeClass('active');
+            }
+        })
+    } else {
+        $('.btn-menu').click(function(){
+            if(!$(this).hasClass('active')){
+                $(this).addClass('active');
+    
+                $('.gnb').addClass('active');
+            } else {
+                $(this).removeClass('active');
+                $('.gnb').removeClass('active');
+            }
         })
     }
 }
